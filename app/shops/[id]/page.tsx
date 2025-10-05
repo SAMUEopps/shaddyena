700d8ef77e351d449c5bcf06688d9d976bda58e0
Samuel Hope <samuelhope805@gmail.com>
2025-10-05 13:12:15 +0300

    first commit

--- app/shops/[id]/page.tsx
+++ app/shops/[id]/page.tsx
@@ -565,8 +565,10 @@ export default function ShopDetailPage() {
 
   if (loading) {
     return (
+      <div className='bg-white'>
       <div className="min-h-screen flex items-center justify-center">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#bf2c7e]" />
+      </div>
       </div>
     );
   }
