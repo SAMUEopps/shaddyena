1685d2e1c4bd89b306e9bb2ee7ebe448f639786c
Samuel Hope <samuelhope805@gmail.com>
2026-07-22 16:51:26 +0300

    api fix

--- app/api/shd-api/api/events/route.ts
+++ app/api/shd-api/api/events/route.ts
@@ -27,9 +27,9 @@
 
 
 // C:\Users\Administrator\Desktop\my-app\app\api\events\route.ts
+import { connectToDatabase } from '@/shd-lib/lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
-import { connectToDatabase } from '@/lib/mongodb';
-import Event from '@/models/Event';
+import Event from '@/shd-models/models/Event';
 
 export async function GET(req: NextRequest) {
   try {
